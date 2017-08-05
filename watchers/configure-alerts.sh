258dc12e985c6b6482c17bac79fe365c0e8cb754
Manan Kalra <manankalra29@gmail.com>
2017-08-05 23:44:23 +0530

    Update configure-alerts.sh

--- watchers/configure-alerts.sh
+++ watchers/configure-alerts.sh
@@ -3,11 +3,10 @@
 # Replace <usernmae> and <password>with your own credentials
 
 # if cpu usage goes above 50%
-sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @cpu-watch.json http://localhost:9200/_xpack/watcher/watch/cpu_elk-117
+sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @cpu-watch.json http://localhost:9200/_xpack/watcher/watch/cpu_watch
 
 # if memory usage goes above 50%
-sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @memory-watch.json http://localhost:9200/_xpack/watcher/watch/memory_elk-117
+sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @memory-watch.json http://localhost:9200/_xpack/watcher/watch/memory_watch
 
 # if disk usage goes above 50%
-sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @filesystem-watch.json http://localhost:9200/_xpack/watcher/watch/filesystem_elk-117
-
+sudo curl --user <username>:<password> -XPUT -H 'Content-Type:application/json' -H 'Accept: application/json' --data-binary @filesystem-watch.json http://localhost:9200/_xpack/watcher/watch/filesystem_watch
